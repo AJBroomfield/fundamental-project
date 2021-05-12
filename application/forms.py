2eceafbeb69b62aa8932c32742b40345612702b7
Andrew Broomfield <abroomfield@qa.com>
2021-05-12 11:50:52 +0000

    added CheckDiceRoll validator

--- application/forms.py
+++ application/forms.py
@@ -9,13 +9,20 @@ class CharacterForm(FlaskForm):
     race = StringField('Character Race', validators=[DataRequired()])
     submit = SubmitField('Add Character')
 
+class CheckDiceRoll:
+    def __init__(self, message='Result is too large'):
+        self.message = message
+    def __call__(self, form, field):
+        if not (field.data <= int(form.dice_roll.data)):
+            raise ValidationError(self.message)
+
 
 class DiceRollForm(FlaskForm):
     character = SelectField('Character Name', choices=[])
     dice_roll = SelectField('Dice Rolled', choices=[(4, 'd4'),(6,'d6'),(8,'d8'),(10,'d10'),(20,'d20')])
     dice_result = IntegerField('Dice Result', validators=[
         DataRequired(), 
-        NumberRange(min=1,max=20,message='Enter a valid result')
+        CheckDiceRoll()
     ])
     submit = SubmitField('Add Roll')
     
